b5bb1d014af8bf15a0d4eb9cfcd52441dd0fa5e9
Kotet <kotet0418@gmail.com>
2019-02-14 16:25:37 +0900

    fractal brownian motion

--- tinykaboom.d
+++ tinykaboom.d
@@ -1,6 +1,6 @@
 import std.stdio;
 import std.algorithm : min, max;
-import std.math : tan, sqrt, PI, sin;
+import std.math : tan, sqrt, PI, sin, floor;
 import std.parallelism : parallel;
 import std.range : iota;
 
@@ -9,7 +9,7 @@ enum light_position = [10, 10, 10];
 enum sphere_radius = 1.5;
 enum sphere_color = [1, 1, 1];
 enum background_color = [0.2, 0.7, 0.8];
-enum noise_amplitude = 0.2;
+enum noise_amplitude = 1;
 
 enum trace_limit = 128;
 enum trace_eps = 0.01;
@@ -21,6 +21,57 @@ enum fov = PI / 3.0;
 enum eps = 0.1;
 
 alias vec3f = float[3];
+
+// aからbまで線形補間
+float lerp(float a, float b, float t)
+{
+    return a + (b - a) * max(0, min(1, t));
+}
+
+float hash(float n)
+{
+    float x = sin(n) * 43758.5453;
+    float h = x - floor(x);
+    assert(0 <= h && h <= 1);
+    return h;
+}
+
+float noise(vec3f x)
+{
+    vec3f p = [floor(x[0]), floor(x[1]), floor(x[2])];
+    vec3f f = [x[0] - p[0], x[1] - p[1], x[1] - p[1]];
+    f = f[] * [3 - f[0] * 2, 3 - f[1] * 2, 3 - f[2] * 2].dotp(f);
+    float n = [1, 57, 113].dotp(p);
+    return lerp(lerp(lerp(hash(n + 0), hash(n + 1), f[0]), lerp(hash(n + 57),
+            hash(n + 58), f[0]), f[1]), lerp(lerp(hash(n + 113), hash(n + 114),
+            f[0]), lerp(hash(n + 170), hash(n + 171), f[0]), f[1]), f[2]);
+}
+
+vec3f rotate(vec3f v)
+{
+    return [[0.00, 0.80, 0.60].dotp(v), [-0.80, 0.36, -0.48].dotp(v),
+        [-0.60, -0.48, 0.64].dotp(v)];
+}
+
+float fractalBrownianNotion(vec3f x)
+{
+    float f = 0;
+
+    vec3f p = rotate(x);
+    // vec3f p = x;
+    f += 0.5000 * noise(p);
+
+    p = p[] * 2.32;
+    f += 0.2500 * noise(p);
+
+    p = p[] * 3.03;
+    f += 0.1250 * noise(p);
+
+    p = p[] * 2.61;
+    f += 0.0625 * noise(p);
+
+    return f / 0.9375;
+}
 
 vec3f normalize(vec3f v)
 {
@@ -43,12 +94,16 @@ float norm(vec3f v)
 
 float signedDistance(vec3f p)
 {
-    float displacement = sin(p[0] * 16) * sin(p[1] * 16) * sin(p[2] * 16) * noise_amplitude;
+    float displacement = -fractalBrownianNotion([p[0] * 3.4, p[1] * 3.4, p[2] * 3.4,])
+        * noise_amplitude;
     return p.norm() - (sphere_radius + displacement);
 }
 
 bool traceSphere(vec3f orig, vec3f dir, ref vec3f pos)
 {
+    bool b = dotp(orig, orig) - dotp(orig, dir) ^^ 2 > sphere_radius ^^ 2;
+    if (b)
+        return false;
     pos = orig;
     foreach (i; 0 .. trace_limit)
     {
